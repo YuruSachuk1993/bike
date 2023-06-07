cd12a5c309f817386024a1a38ced6014f4c0da27
YuruSachyk1993 <375292068972cv@gmail.com>
2023-06-07 22:54:05 +0300

    07,06,23доверстать

--- js/file.js
+++ js/file.js
@@ -3,3 +3,25 @@ $('.icon-menu').click(function (event) {
    $('.menu__body').toggleClass('active');
    $('body').toggleClass('lock');
 });
+
+$(document).ready(function () {
+   $('.slider__body').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      
+
+      arrows: false,
+      accessibility: false,
+      autoplaySpeed: 3000,
+      adaptiveHeight: true,
+      nextArrow: '<button type="button" class="slick-next"></button>',
+      prevArrow: '<button type="button" class="slick-prev"></button>',
+      responsive: [{
+         dreakpoint: 768,
+         settings: {}
+      }]
+
+
+   });
+});
